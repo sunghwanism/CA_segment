ce51ebed492a77198c58495faa189df584aa4024
sunghwanism <msh2044@gmail.com>
2025-12-30 21:18:42 -0500

    load cfg and load scan function

--- dataset/common.py
+++ dataset/common.py
@@ -1,7 +1,10 @@
 
 import ast
 import pandas as pd
+import pydicom
 
+from utils.functions import load_config
+cfg = load_config()
 
 def load_patient_data(PATH):
     df = pd.read_csv(PATH)
@@ -16,3 +19,8 @@ def load_patient_data(PATH):
         df['Direction'] = df['Direction'].apply(ast.literal_eval)
 
     return df
+
+def load_scan(FileList):
+    slices = [pydicom.dcmread(cfg.paths.raw_data + s) for s in FileList]
+    
+    return slices
