5e0727e71c0567d921621352414b2e335089abb4
sunghwanism <msh2044@gmail.com>
2025-12-31 20:45:57 -0500

    init setup version 0.1

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 setup(
     name="CA-Segmentation",
     version="0.1",
-    packages=find_packages(), # __init__.py가 있는 모든 폴더를 패키지로 인식
+    packages=find_packages(),
 )
